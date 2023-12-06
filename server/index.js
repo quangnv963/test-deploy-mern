3c100d765fc330b4a3a26b9112acfad3fd22a704
quangnv963 <nguyenquang090603@gmail.com>
2023-12-06 22:23:38 +0700

    fix mongo URL

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(cors(
 ));
 app.use(express.json())
 
-mongoose.connect('mongodb+srv://yousaf:test123@cluster0.g4i5dey.mongodb.net/test?retryWrites=true&w=majority');
+mongoose.connect('mongodb+srv://test:cuong123456@companycv.rr6odbl.mongodb.net/companycv');
 
 
 app.get("/", (req, res) => {
