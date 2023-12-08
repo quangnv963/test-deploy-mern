88195a616c2e0207b952d800bc5e550335af1b42
quangnv963 <nguyenquang090603@gmail.com>
2023-12-08 19:23:48 +0700

    push api

--- frontend/src/api/instance.tsx
+++ frontend/src/api/instance.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'https://test-deploy-mern-omega.vercel.app/api/',
     timeout:1000,
     headers:{"Content-Type":"application/json",}
 })
